3ccd72f862ba26cc4f7e40d793efa5c9bb7f577f
Patience-Kori1 <koribiram@yahoo.fr>
2025-07-29 13:27:31 +0200

    feat(categorie): Initial category CRUD + styled creation form - Created CategorieController with related routes and actions - Added action to list all categories using findAll - Created view for category listing - Added action and view to create a new category with a form - Styled the category creation form using Bootstrap

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use App\Form\CategoryType;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class CategorieController extends AbstractController
 {
     #[Route('/allCategories', name: 'app_allCategories')]
-    public function index(): Response
+    public function allCategories (CategorieRepository $repo): Response
     {
+        $categories = $repo->findAll();
+        
         return $this->render('categorie/allCategories.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function addCategory(EntityManagerInterface $em, Request $request): Response
+    {
+        $category = new Categorie();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash('success', 'La catégorie a bien été ajouté');
+            return $this->redirectToRoute('app_home_page');
+        }
+
+        return $this->render('categorie/newCategorie.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
