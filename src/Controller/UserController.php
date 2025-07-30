8cd149bdf9d33f3ca9b38a795c23c5730f262a46
Patience-Kori1 <koribiram@yahoo.fr>
2025-07-31 01:45:05 +0200

    feat(user): Add editor role assignment feature - Created method and route to assign 'editor' role to a user - Added a button in the users view to trigger role assignment - Condition: if user is not admin, show Edit/Delete buttons - If user is admin, display a 'I am admin' button instead - Added onclick confirmation dialog before role assignment - Flash message added upon successful role update - Flash message included in the template using include

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
-    #[Route('/users', name: 'app_user')]
+    #[Route('/admin/user', name: 'app_user')]
     public function user (UserRepository $repo): Response
     {
         $users= $repo->findAll();
         return $this->render('user/users.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    #[Route('/admin/user/{id}/to/editor ', name: 'app_user_to_editor')] //remettre la wildcard {role} pour selection du role en question
+    public function changeRole(EntityManagerInterface $entityManager, User $user): Response
+    {
+        $user->setRoles(['ROLE_EDITOR', 'ROLE_USER']);
+        $entityManager->flush();
+
+        $this->addFlash('success', "Le rôle éditeur à bien été ajouté à l'utilisateur");
+
+        return $this->redirectToRoute('app_user');
     }
 }
