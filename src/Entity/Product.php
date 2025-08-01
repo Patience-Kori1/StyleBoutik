6c06c69ec22c2059cdac923a81300b679e8f849a
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-01 08:46:21 +0200

    feat(product): Created Product entity linked to SubCategory - Generated full CRUD using make:crud command - Linked Product to SubCategory via foreign key - Started styling the generated views (in progress)

---
+++ src/Entity/Product.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    /**
+     * @var Collection<int, SubCategory>
+     */
+    #[ORM\ManyToMany(targetEntity: SubCategory::class, inversedBy: 'products')]
+    private Collection $subcategory;
+
+    public function __construct()
+    {
+        $this->subcategory = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, SubCategory>
+     */
+    public function getSubcategory(): Collection
+    {
+        return $this->subcategory;
+    }
+
+    public function addSubcategory(SubCategory $subcategory): static
+    {
+        if (!$this->subcategory->contains($subcategory)) {
+            $this->subcategory->add($subcategory);
+        }
+
+        return $this;
+    }
+
+    public function removeSubcategory(SubCategory $subcategory): static
+    {
+        $this->subcategory->removeElement($subcategory);
+
+        return $this;
+    }
+}
