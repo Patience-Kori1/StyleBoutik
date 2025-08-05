1f1f3d1710374cdd27c231ed325662e0b44cf334
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-05 09:39:06 +0200

    feat(home): Display products dynamically on the homepage - Retrieved products in the controller using the findAll() method - Passed the products to the view via the 'products' variable - Used a for loop in home/index.html.twig to display products - Added conditional display of buttons based on admin role - Styled the page for better product presentation

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(ProductRepository $productRepo): Response
     {
+        $products= $productRepo->findAll();
+       
         return $this->render('home_page/homePage.html.twig', [
-            'controller_name' => 'HomePageController',
+            'products' => $products,
         ]);
     }
 }
