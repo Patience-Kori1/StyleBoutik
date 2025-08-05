4a3d162ae3ee2a1d4721f75fe25a626baac034d7
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-05 17:17:14 +0200

    feat(homepage): Pagination of the product list on the homepage - Installed KnpPaginator bundle for pagination management - Injected PaginatorInterface service into HomePageController - Implemented pagination logic (8 products per page) - Displayed navigation buttons using knp_pagination_render() - Created and customized pagination_template.html.twig for button styling

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,18 +5,25 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use App\Repository\CategorieRepository;
 use App\Repository\SubCategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function homePage(ProductRepository $productRepo, CategorieRepository $categoryRepo): Response
+    public function homePage(ProductRepository $productRepo, CategorieRepository $categoryRepo, Request $request, PaginatorInterface $paginator): Response
     {
         $products= $productRepo->findAll();
         $category = $categoryRepo->findAll();
-       
+        $data = $productRepo->findby([],['id'=>"DESC"]);
+        $products = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),//met en place la pagination
+            8 //je choisi la limite de 8 articles par page
+        );
         return $this->render('home_page/homePage.html.twig', [
             'products' => $products,
             'category' => $category,
