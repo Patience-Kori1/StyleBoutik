8fe78dc7e34d3fe59f6cf8afd7fc52fb59f30efc
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-06 20:59:27 +0200

    feat(order): Order form with delivery fees based on selected city - Created OrderType form to manage order validation - Retrieved cart data in OrderController - Displayed and started styling the order form in order/index.html.twig - Integrated total amount from the cart into the view - Imported jQuery to dynamically handle city select input - Prepared logic to manage delivery fees based on selected city

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 final class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    public function index(Request $request, SessionInterface $session, ProductRepository $productRepo): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        $cart= $session->get('cart',[]);
+        $cartWithData = [];
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'product' => $productRepo->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        //Calcul du total du panier  
+        $total = array_sum(array_map(function($item){
+            return $item['product']->getPrice()* $item['quantity'];
+        },$cartWithData));
+        // dd($cartWithData);
+
+        $order= new Order;
+        $form= $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+        return $this->render('order/orderIndex.html.twig', [
+            'form'=>$form->createView(),
+            'total'=> $total
         ]);
     }
 }
