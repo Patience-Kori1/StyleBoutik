a5ee547bcf48049410b2bb6b8aa4cec0feb5d5df
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-25 11:40:40 +0200

    feat(bill): Finalized PDF invoice generation - Installed dompdf/dompdf package for PDF rendering - Added BillController logic with Dompdf configuration - Integrated Twig view indexBill.html.twig for invoice rendering - Removed base.html.twig extension to exclude navbar from invoice

--- src/Controller/BillController.php
+++ src/Controller/BillController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use App\Repository\OrderRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BillController extends AbstractController
 {
@@ -13,8 +15,22 @@ final class BillController extends AbstractController
     public function index($id, OrderRepository $orderRepo): Response
     {
         $order = $orderRepo->find($id);
-        return $this->render('bill/indexBill.html.twig', [
-            'order' => $order,
+
+        $pdfOptions = new Options(); //definit la nouvelle instanciation de classe Options de Dompdf
+        $pdfOptions->set('defaultFont','Arial'); //Définit la font
+        $domPdf = new Dompdf($pdfOptions);//On ajoute les options
+        $html = $this->renderView('bill/indexBill.html.twig', [
+            'order'=>$order,
+         ]); // On insere ce que l'on veut imprimer
+        $domPdf->loadHtml($html); // On charge le html dans dompdf
+        $domPdf->render(); //On crée le rendu
+        $domPdf->stream('SneakHub-Facture-'.$order->getId().'.pdf',[//On concatene la facture avec la terminaison"pdf"
+            'Attachment'=>false //ca permet de dire on va telecharger le fichier, ou l'afficher et decider de l'imprimer et telecharger
+        ]); 
+        
+        return new Response('',200,[ //
+            'Content-Type' => 'application/pdf' 
         ]);
+
     }
 }
