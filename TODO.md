cbcc8db75ae0c34ecd0f0d92cd93654933c1fc49
Patience-Kori1 <koribiram@yahoo.fr>
2025-08-25 10:45:21 +0200

    feat(bill): Start of invoice implementation - Created a dedicated controller for invoice management - Added indexBill.html.twig view with order, customer, and product details - Used inline CSS since Dompdf does not support Bootstrap - Added a button in orders.html.twig to access invoice - Added product image column in orders.html.twig - Created a TODO file for tracking progress

---
+++ TODO.md
@@ -0,0 +1,13 @@
+
+
+M : Must (have)
+S : Should (have)
+C : Could (have)
+W: Wont (have)
+
+#TODO
+
+S: Gestion de stock
+C: Tarte au citron
+
+Afficher les fichiers .md en propre : Ctrl + Shift + Y
---
+++ src/Controller/BillController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\OrderRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BillController extends AbstractController
+{
+    #[Route('/editor/order/{id}/bill', name: 'app_bill')]
+    public function index($id, OrderRepository $orderRepo): Response
+    {
+        $order = $orderRepo->find($id);
+        return $this->render('bill/indexBill.html.twig', [
+            'order' => $order,
+        ]);
+    }
+}
